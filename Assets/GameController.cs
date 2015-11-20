9c097ae10191205afe8b9c4467ea21b7b4819a36
render14 <aneiluy@gmail.com>
2015-11-20 12:13:01 -0500

    3!

---
+++ Assets/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject cookie;
+	// Use this for initialization
+	void Start () {
+        Instantiate(cookie);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
